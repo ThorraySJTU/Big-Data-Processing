ab4c01887a161138dabfc9a06fd916f253c57e3b
bereinWTC <47750903+bereinWTC@users.noreply.github.com>
2019-11-26 22:05:29 +0800

    Add files via upload
    tree.py defines a tree for the file system. api.py could be used for testing tree.py with functions required: ls, cd, mkdir, rm, etc.
    server.py defines two class: DFSserver and DFSclient. They could connect to each other via a proper port. The tree defined in tree.py is imported in the file. However, it does not require api.py. Functions are re-write in the class DFSserver to suit its format.
    
    Finished: tree. definition of all functions. functions of the server. 
    To be done: functions and connection of the clients. connections need to be tested.

---
+++ tree.py
@@ -0,0 +1,87 @@
+class TreeNode:
+
+    def __init__(self, name, parent=None):
+        super(TreeNode, self).__init__()
+        self.name = name
+        self.parent = parent
+        self.child = {}
+
+    def get_child(self, name):
+        return self.child.get(name)
+
+    def get_parent(self):
+        return self.parent
+
+    def add_child(self, obj):
+        if isinstance(obj, TreeNode):
+            self.child[obj.name] = obj
+            obj.parent = self
+        else:
+            self.child[obj] = None
+
+    def del_child(self, name):
+        if name in self.child:
+            del self.child[name]
+
+    def arrive_node(self, path, create=False):
+        path = path if isinstance(path, list) else path.split()
+        current = self
+        for i in path:
+            if i not in current.child.keys():
+                if create:
+                    current.add_child(i)
+                else:
+                    return None
+            current = current.get_child(i)
+
+        return current
+
+    def show_tree(self):
+        def print_branch(treenode, degree):
+            if isinstance(treenode, TreeNode):
+                for item in treenode.child.keys():
+                    print((degree + 1) * '\t' + item)
+                    print_branch(treenode.get_child(item), degree + 1)
+
+        print(self.name)
+        print_branch(self, 0)
+
+    def list_children(self):
+        for item in self.child.keys():
+            print(item)
+
+    def print_path(self):
+        path = []
+        current = self
+        while current != None:
+            path.append(current.name)
+            current = current.parent
+        for i in range(len(path)):
+            print(i * '\t' + path[-(i+1)])
+
+
+if __name__ == '__main__':
+    test = TreeNode('~')
+    test.add_child('level_1_file')
+    test.add_child(TreeNode('level_1_dic'))
+    res = test.arrive_node(['level_1_dic'])
+    res.add_child(TreeNode('level_2_dic'))
+    print('Test add and arrive')
+    test.show_tree()
+    print('---------------')
+    print('Test arrive exception')
+    tmp = test.arrive_node(['level_?_dic'])
+    print(tmp)
+    print('---------------')
+    print('Test list children')
+    test.list_children()
+    # print(res.arrive_node('level_2_dic').get_parent().name)
+    print('---------------')
+    print('Test pwd')
+    res.print_path()
+    res = res.arrive_node('level_2_dic')
+    res.print_path()
+    print('---------------')
+    print('Test del')
+    test.del_child('level_1_dic')
+    test.show_tree()
